f3f8cc13c4a2e5aca5aa4eafb934b86408e13e35
akhzar nazir <akhzarn@yahoo.com>
2018-07-17 22:49:38 +0500

    Hello

--- ARKitExample/ViewController.swift
+++ ARKitExample/ViewController.swift
@@ -9,10 +9,9 @@
 import ARKit
 import AVFoundation
 
-let kStartingPosition = SCNVector3(0.0, 0.0, -0.6)
-let kAnimationDurationMoving: TimeInterval = 0.2
-let kMovingLengthPerLoop: CGFloat = 0.05
-let kRotationRadianPerLoop: CGFloat = 0.2
+var kAnimationDurationMoving: TimeInterval = 0.2
+var kMovingLengthPerLoop: CGFloat = 0.05
+var kRotationRadianPerLoop: CGFloat = 0.2
 var toggleStateDroneOnOff = 2
 var toggleStatelightOnOff = 2
 
@@ -23,7 +22,7 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     var player: AVPlayer?
     var isVideoFinish : Bool = false
     var flagThrottleUp:Int = 1
-    var doubleflagThrottleUp:Double = 0.0
+//    var doubleflagThrottleUp:Double = 0.0
 
     @IBOutlet weak var dronOffView: UIView!
 
@@ -32,9 +31,6 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     @IBOutlet weak var questionView: UIView!
     
     @IBOutlet weak var topHeaderView: UIView!
-    @IBOutlet weak var btn3: UIButton!
-    @IBOutlet weak var btn2: UIButton!
-    @IBOutlet weak var btn1: UIButton!
     @IBOutlet weak var viewBottomLeft: UIView!
     @IBOutlet weak var viewBottomRight: UIView!
     @IBOutlet weak var viewBottomCenter: UIView!
@@ -48,6 +44,7 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     @IBOutlet weak var sceneView: ARSCNView!
     
     @IBOutlet weak var imageRace: UIImageView!
+    
     var grids = [Grid]()
     let drone = SCNNode()
 
@@ -62,18 +59,69 @@ class ViewController: UIViewController , ARSCNViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setupScene()
-        configureLighting()
+        setUpBasicConfiguration()
+    }
+    
+    func setUpBasicConfiguration() {
         self.questionView.isHidden = true
-        btn1.layer.cornerRadius = btn1.frame.width / 2
-        btn1.clipsToBounds = true
-        btn2.layer.cornerRadius = btn1.frame.width / 2
-        btn2.clipsToBounds = true
-        btn3.layer.cornerRadius = btn1.frame.width / 2
-        btn3.clipsToBounds = true
         self.view.addSubview(self.videoViewContainer)
         initializeVideoPlayerWithVideo()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpSceneView()
+    }
+    
+    func setUpSceneView() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal,.vertical]
+        sceneView.session.run(configuration)
+        sceneView.delegate = self
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    }
+    
+    @IBAction func restartActionScan(_ sender: Any) {
         
+        self.grids.removeAll()
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.pause()
+        sceneView.scene.rootNode.enumerateChildNodes { (node, stop) in
+        node.removeFromParentNode()
+        }
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+        self.questionView.isHidden = true
+        
+        configuration.planeDetection = [.horizontal,.vertical]
+        sceneView.session.run(configuration)
+        sceneView.delegate = self
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        kAnimationDurationMoving = 0.2
+        kRotationRadianPerLoop = 0.2
+        toggleStateDroneOnOff = 2
+        toggleStatelightOnOff = 2
+        flagThrottleUp = 1
+//        doubleflagThrottleUp = 0.0
+        
+        planeAnchor = nil
+        
+        self.imgLightsOnOff.image = UIImage(named: "lightsoff")
+        self.imgDroneOnOff.image = UIImage(named:"startdroneoff")
+        
+        let strImageName : String = "\(flagThrottleUp).png"
+        self.imageRace.image = UIImage(named:strImageName)!
+
+        self.drone.removeFromParentNode()
+
+        self.viewBottomLeft.isHidden = true
+        self.imageRace.isHidden = true
+        self.dronOffView.isHidden = true
+        self.viewBottomRight.isHidden = true
+        self.viewBottomCenter.isHidden = true
+        self.topHeaderView.isHidden = false
+        self.questionView.isHidden = true
     }
     
     func initializeVideoPlayerWithVideo() {
@@ -97,24 +145,9 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     }
     
     @objc func playerDidFinishPlaying(note: NSNotification){
-        print("Video Finished")
         self.isVideoFinish = true
         self.videoViewContainer.isHidden = true
     }
-    
-    @IBAction func restartActionScan(_ sender: Any) {
-//        self.grids.removeAll()
-//        let configuration = ARWorldTrackingConfiguration()
-//
-//        sceneView.session.pause()
-//        sceneView.scene.rootNode.enumerateChildNodes { (node, stop) in
-//            node.removeFromParentNode()
-//        }
-//        
-//        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-//
-//        setUpSceneView()
-}
     
     override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
         if (UIDevice.current.orientation.isLandscape) {
@@ -143,56 +176,6 @@ class ViewController: UIViewController , ARSCNViewDelegate {
            // controllsContainerView.frame = videoContainerView.bounds
             self.layer?.removeFromSuperlayer()
         }
-    }
-        
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-//        setupConfiguration()
-        
-//        let imgListArray :NSMutableArray = []
-//        for countValue in 1...10
-//        {
-//
-//            let strImageName : String = "\(countValue).png"
-//            let image  = UIImage(named:strImageName)
-//            imgListArray .add(image!)
-//        }
-//
-//        self.imageRace.animationImages = imgListArray as? [UIImage]
-//        self.imageRace.animationDuration = 3.0
-//        self.imageRace.startAnimating()
-        
-        //self.imageRace.stopAnimating()
-        
-        
-        setUpSceneView()
-    }
-    
-    func configureLighting() {
-//        sceneView.autoenablesDefaultLighting = true
-//        sceneView.automaticallyUpdatesLighting = true
-    }
-
-    // MARK: - setup
-    //    func setupScene() {
-    //        let scene = SCNScene()
-    //        sceneView.scene = scene
-    //        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
-    //    }
-    //
-    //    func setupConfiguration() {
-    //                let configuration = ARWorldTrackingConfiguration()
-    //                sceneView.session.run(configuration)
-    //                configuration.planeDetection = .horizontal
-    //    }
-    
-    func setUpSceneView() {
-        let configuration = ARWorldTrackingConfiguration()
-        configuration.planeDetection = [.horizontal,.vertical]
-//        configuration.isLightEstimationEnabled = true
-        sceneView.session.run(configuration)
-        sceneView.delegate = self
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
     @IBAction func loadDroneAction(_ sender: Any) {
@@ -264,8 +247,6 @@ class ViewController: UIViewController , ARSCNViewDelegate {
         }
     }
     
-    
-    
     // End
     
     private func execute(action: SCNAction, sender: UILongPressGestureRecognizer) {
@@ -276,12 +257,28 @@ class ViewController: UIViewController , ARSCNViewDelegate {
             drone.runAction(loopAction)
         } else if sender.state == .ended {
 //            self.imgLeftPressed.backgroundColor = UIColor.white
-
             drone.removeAllActions()
         }
     }
     
-    @IBAction func throttleUpLongPressed(_ sender: Any) {
+    @IBAction func ThrottleupLongPressed(_ sender: UILongPressGestureRecognizer) {
+        
+//        doubleflagThrottleUp = Double(flagThrottleUp/2)
+//        if doubleflagThrottleUp == 0{
+//            doubleflagThrottleUp = 0.5
+//        }
+        
+        let action = SCNAction.moveBy(x: 0, y: kMovingLengthPerLoop, z: 0, duration: kAnimationDurationMoving)
+        //        execute(action: action, sender: sender)
+        let loopAction = SCNAction.repeatForever(action)
+        if sender.state == .began {
+            self.imgSimple.image = UIImage(named: "top")
+            drone.runAction(loopAction)
+        } else if sender.state == .ended {
+            self.imgSimple.image = UIImage(named: "simple")
+            drone.removeAllActions()
+        }
+        
         var image:UIImage? = nil
         if self.flagThrottleUp<=9{
             flagThrottleUp+=1
@@ -289,13 +286,37 @@ class ViewController: UIViewController , ARSCNViewDelegate {
             image = UIImage(named:strImageName)!
             self.imageRace.image = image
         }else{
-        let strImageName : String = "\(10).png"
-        image = UIImage(named:strImageName)!
-        self.imageRace.image = image
+            let strImageName : String = "\(10).png"
+            image = UIImage(named:strImageName)!
+            self.imageRace.image = image
         }
     }
     
-    @IBAction func throttleDownLongPressed(_ sender: Any) {
+    @IBAction func ThrottleDownLongPressed(_ sender: UILongPressGestureRecognizer) {
+        
+//        doubleflagThrottleUp = Double(flagThrottleUp/2)
+//        if doubleflagThrottleUp == 0{
+//            doubleflagThrottleUp = 0.5
+//        }
+        
+        let action = SCNAction.moveBy(x: 0, y: -0.005, z: 0, duration:kAnimationDurationMoving)
+        //        execute(action: action, sender: sender)
+        let loopAction = SCNAction.repeatForever(action)
+        if sender.state == .changed {
+            print(drone.position.y)
+            print(planeAnchor?.transform.columns.3.y ?? 0)
+            if drone.position.y>(planeAnchor?.transform.columns.3.y)!{
+                self.imgSimple.image = UIImage(named: "bottom")
+                drone.runAction(loopAction)
+            }else{
+                self.imgSimple.image = UIImage(named: "simple")
+                drone.removeAllActions()
+            }
+        } else if sender.state == .ended {
+            self.imgSimple.image = UIImage(named: "simple")
+            drone.removeAllActions()
+        }
+        
         var image:UIImage? = nil
         if self.flagThrottleUp>=2{
             flagThrottleUp-=1
@@ -307,15 +328,44 @@ class ViewController: UIViewController , ARSCNViewDelegate {
             image = UIImage(named:strImageName)!
             self.imageRace.image = image
         }
-        
     }
+    
+//    @IBAction func throttleUpLongPressed(_ sender: Any) {
+//        var image:UIImage? = nil
+//        if self.flagThrottleUp<=9{
+//            flagThrottleUp+=1
+//            let strImageName : String = "\(flagThrottleUp).png"
+//            image = UIImage(named:strImageName)!
+//            self.imageRace.image = image
+//        }else{
+//        let strImageName : String = "\(10).png"
+//        image = UIImage(named:strImageName)!
+//        self.imageRace.image = image
+//        }
+//    }
+    
+//    @IBAction func throttleDownLongPressed(_ sender: Any) {
+//        var image:UIImage? = nil
+//        if self.flagThrottleUp>=2{
+//            flagThrottleUp-=1
+//            let strImageName : String = "\(flagThrottleUp).png"
+//            image = UIImage(named:strImageName)!
+//            self.imageRace.image = image
+//        }else{
+//            let strImageName : String = "\(1).png"
+//            image = UIImage(named:strImageName)!
+//            self.imageRace.image = image
+//        }
+//    }
 
     @IBAction func upLongPressed(_ sender: UILongPressGestureRecognizer) {
-        doubleflagThrottleUp = Double(flagThrottleUp/2)
-        if doubleflagThrottleUp == 0{
-            doubleflagThrottleUp = 0.5
-        }
-        let action = SCNAction.moveBy(x: 0, y: kMovingLengthPerLoop, z: 0, duration: kAnimationDurationMoving/doubleflagThrottleUp)
+        
+//        doubleflagThrottleUp = Double(flagThrottleUp/2)
+//        if doubleflagThrottleUp == 0{
+//            doubleflagThrottleUp = 0.5
+//        }
+        
+        let action = SCNAction.moveBy(x: 0, y: kMovingLengthPerLoop, z: 0, duration: kAnimationDurationMoving)
 //        execute(action: action, sender: sender)
         let loopAction = SCNAction.repeatForever(action)
         if sender.state == .began {
@@ -329,11 +379,13 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     }
     
     @IBAction func downLongPressed(_ sender: UILongPressGestureRecognizer) {
-        doubleflagThrottleUp = Double(flagThrottleUp/2)
-        if doubleflagThrottleUp == 0{
-            doubleflagThrottleUp = 0.5
-        }
-        let action = SCNAction.moveBy(x: 0, y: -0.005, z: 0, duration:kAnimationDurationMoving/doubleflagThrottleUp)
+       
+//        doubleflagThrottleUp = Double(flagThrottleUp/2)
+//        if doubleflagThrottleUp == 0{
+//            doubleflagThrottleUp = 0.5
+//        }
+        
+        let action = SCNAction.moveBy(x: 0, y: -0.005, z: 0, duration:kAnimationDurationMoving)
 //        execute(action: action, sender: sender)
         let loopAction = SCNAction.repeatForever(action)
         if sender.state == .changed {
@@ -350,19 +402,21 @@ class ViewController: UIViewController , ARSCNViewDelegate {
             self.imgSimple.image = UIImage(named: "simple")
             drone.removeAllActions()
         }
+        
     }
     
     @IBAction func moveLeftLongPressed(_ sender: UILongPressGestureRecognizer) {
         
+//        doubleflagThrottleUp = Double(flagThrottleUp/2)
+//        if doubleflagThrottleUp == 0{
+//            doubleflagThrottleUp = 0.5
+//        }
+
         let x = -deltas().cos
         let z = deltas().sin
+        
 //        moveDrone(x: x, z: z, sender: sender)
-        doubleflagThrottleUp = Double(flagThrottleUp/2)
-        if doubleflagThrottleUp == 0{
-            doubleflagThrottleUp = 0.5
-        }
-//        moveDrone(x: x, z: z, sender: sender)
-        let action = SCNAction.moveBy(x: x, y: 0, z: z, duration:kAnimationDurationMoving/doubleflagThrottleUp)
+        let action = SCNAction.moveBy(x: x, y: 0, z: z, duration:kAnimationDurationMoving)
 //        execute(action: action, sender: sender)
         let loopAction = SCNAction.repeatForever(action)
         if sender.state == .began {
@@ -375,14 +429,16 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     }
     
     @IBAction func moveRightLongPressed(_ sender: UILongPressGestureRecognizer) {
+        
+//        doubleflagThrottleUp = Double(flagThrottleUp/2)
+//        if doubleflagThrottleUp == 0{
+//            doubleflagThrottleUp = 0.5
+//        }
+        
         let x = deltas().cos
         let z = -deltas().sin
-        doubleflagThrottleUp = Double(flagThrottleUp/2)
-        if doubleflagThrottleUp == 0{
-            doubleflagThrottleUp = 0.5
-        }
 //        moveDrone(x: x, z: z, sender: sender)
-        let action = SCNAction.moveBy(x: x, y: 0, z: z, duration:kAnimationDurationMoving/doubleflagThrottleUp)
+        let action = SCNAction.moveBy(x: x, y: 0, z: z, duration:kAnimationDurationMoving)
 //        execute(action: action, sender: sender)
         let loopAction = SCNAction.repeatForever(action)
         if sender.state == .began {
@@ -451,15 +507,14 @@ class ViewController: UIViewController , ARSCNViewDelegate {
             self.planeAnchor = planeAnchor
             // Clear out the debugging options once a plane has been detected
             self.sceneView.debugOptions = []
-            self.loadDragonScene(with: planeAnchor)
+            self.loadDroneScene(with: planeAnchor)
             let grid = Grid(anchor: anchor as! ARPlaneAnchor)
             self.grids.append(grid)
             node.addChildNode(grid)
         }
-        
     }
     
-    func loadDragonScene(with anchor: ARPlaneAnchor) {
+    func loadDroneScene(with anchor: ARPlaneAnchor) {
         let dragonScene = SCNScene(named: "001_Drone.dae")!
         let positionAnchor = anchor.transform
         for childNode in dragonScene.rootNode.childNodes {
@@ -467,61 +522,11 @@ class ViewController: UIViewController , ARSCNViewDelegate {
             self.drone.addChildNode(childNode)
         }
         
-//        if self.drone.name == "Omni001"{
-//            self.drone.light?.intensity = 1000.0
-//        }
-//
-//        if self.drone.name == "Omni002"{
-//            self.drone.light?.intensity = 1000.0
-//        }
-//
-//        if self.drone.name == "Omni003"{
-//            self.drone.light?.intensity = 1000.0
-//        }
-//
-//        if self.drone.name == "Omni004"{
-//            self.drone.light?.intensity = 1000.0
-//        }
-        
         let scale:Float = 0.002
         self.drone.scale = SCNVector3(x: scale, y: scale, z: scale)
         self.drone.position = SCNVector3(x: positionAnchor.columns.3.x, y: positionAnchor.columns.3.y+0.05, z: positionAnchor.columns.3.z)
-       
-//        xPos = CGFloat(position.columns.3.x)
-//        yPos = CGFloat(position.columns.3.y)
-//        zPos = CGFloat(position.columns.3.z)
-//        sceneView.scene.rootNode.addChildNode(self.drone)
-
     }
     
-    // MARK: - ARSCNViewDelegate
-    
-    /*
-     // Override to create and configure nodes for anchors added to the view's session.
-     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-     let node = SCNNode()
-     
-     return node
-     }
-     */
-
-//    func session(_ session: ARSession, didFailWithError error: Error) {
-//        // Present an error message to the user
-//    }
-//    
-//    func sessionWasInterrupted(_ session: ARSession) {
-//        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-//    }
-//    
-//    func sessionInterruptionEnded(_ session: ARSession) {
-//        // Reset tracking and/or remove existing anchors if consistent tracking is required
-//    }
-//    
-//    // 1.
- 
-    
-
-// 2.
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         let grid = self.grids.filter { grid in
             return grid.anchor.identifier == anchor.identifier
@@ -537,25 +542,4 @@ class ViewController: UIViewController , ARSCNViewDelegate {
             self.grids.removeAll()
         }
     }
-    
-    
-//    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-//        // Your code goes here
-//        print("YES Detected")
-//
-//        if let anchor = anchor as? ARPlaneAnchor {
-//            // Your code goes here
-//        }
-//
-//        let plane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
-//        plane.materials.first?.diffuse.contents = UIColor.red.withAlphaComponent(0.5)
-//
-//
-//        let planeNode = SCNNode(geometry: plane)
-//
-//        planeNode.position = SCNVector3(CGFloat(anchor.center.x), CGFloat(anchor.center.y), CGFloat(anchor.center.z))
-//        planeNode.eulerAngles.x = -.pi / 2
-//
-//        node.addChildNode(planeNode)
-    
 }
